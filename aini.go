77ba0a1fee16272152a53ee5f01108964293a6fb
Richard Outten (mbp) <outtenr@gmail.com>
2014-11-23 16:16:25 -0500

    Started adding basic parsing
    Basic parsing of the hosts file. Still need to implement another ansible
    host file features.

--- aini.go
+++ aini.go
@@ -4,22 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strings"
+
+	"github.com/flynn/go-shlex"
 )
 
 type Hosts struct {
-	input   *bufio.Reader
-	Groups  []Group
-	Servers []Server
+	input  *bufio.Reader
+	Groups map[string][]Host
 }
 
-type Server struct {
+type Host struct {
 	Name string
+	Port int
 }
 
-type Group struct {
-	Name    string
-	Servers []Server
-}
+// type Group struct {
+// 	Name    string
+// 	Servers []string
+// }
 
 func NewParser(r io.Reader) (*Hosts, error) {
 	input := bufio.NewReader(r)
@@ -29,8 +32,30 @@ func NewParser(r io.Reader) (*Hosts, error) {
 
 func (h *Hosts) Parse() error {
 	scanner := bufio.NewScanner(h.input)
+
+	activeGroupName := "ungrouped"
+	h.Groups = make(map[string][]Host)
+	h.Groups[activeGroupName] = make([]Host, 0)
+
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := strings.Trim(scanner.Text(), " ")
+		// fmt.Println(line)
+
+		if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
+			activeGroupName = strings.Replace(strings.Replace(line, "[", "", -1), "]", "", -1)
+			if _, ok := h.Groups[activeGroupName]; !ok {
+				h.Groups[activeGroupName] = make([]Host, 0)
+			}
+		} else if strings.HasPrefix(line, ";") || line == "" {
+			// do nothing
+		} else if activeGroupName != "" {
+			parts, err := shlex.Split(line)
+			if err != nil {
+				fmt.Println("couldn't tokenizer ", line)
+			}
+			host := Host{Name: parts[0]}
+			h.Groups[activeGroupName] = append(h.Groups[activeGroupName], host)
+		}
 	}
 	return nil
 }
--- aini_test.go
+++ aini_test.go
@@ -5,10 +5,52 @@ import (
 	"testing"
 )
 
-//
-func TestTokenizer(t *testing.T) {
-	i := "myhost1\n[dbs]\ndbhost1"
-	testInput := strings.NewReader(i)
+func input1() string {
+	return `
+myhost1
+[dbs]
+dbhost1
+dbhost2
+
+[apps]
+my-app-server1
+my-app-server2
+
+`
+}
+
+func createHosts(input string) Hosts {
+	testInput := strings.NewReader(input)
 	v, _ := NewParser(testInput)
 	v.Parse()
+	return *v
+}
+
+//
+func TestGroupExists(t *testing.T) {
+	v := createHosts(input1())
+	matched := false
+	if _, ok := v.Groups["dbs"]; ok {
+		matched = true
+	}
+	if !matched {
+		t.Error("Expected to find the group \"dbs\"")
+	}
+}
+
+func TestServerExistsInGroup(t *testing.T) {
+	v := createHosts(input1())
+	if hosts, ok := v.Groups["dbs"]; ok {
+		matched := false
+		for _, host := range hosts {
+			if host.Name == "dbhost2" {
+				matched = true
+			}
+		}
+		if !matched {
+			t.Error("Server dbhost2 was not found in dbs")
+		}
+	} else {
+		t.Error("\"dbs\" group didn't exists")
+	}
 }
